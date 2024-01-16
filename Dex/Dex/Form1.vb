7efc9a8e2de6daa5bf0c59af56d0212e86ab26dc
Oscar <ogar917@learner.dvusd.org>
2024-01-16 13:56:54 -0700

    tuesday 1/16

--- Dex/Dex/Form1.vb
+++ Dex/Dex/Form1.vb
@@ -36,13 +36,25 @@ Public Class Form1
     Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs)
         PictureBox1.Load(OpenFileDialog1.FileName)
     End Sub
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If IO.File.Exists("data.txt") Then
             Dim inFile As New IO.StreamReader("data.txt")
             records(0) = inFile.ReadLine
-            records(1) = inFile.ReadLine
             inFile.Close()
+            showrecord(0)
+        End If
+
+    End Sub
+    Public Sub showrecord(index As Integer)
+        Dim fields() As String
+        fields = records(index).Split("|")
+        F1.Text = fields(0)
+        F2.Text = fields(1)
+        F3.Text = fields(2)
+        F4.Text = fields(3)
+        F5.Text = fields(4)
+        If File.Exists(fields(5)) Then
+            PictureBox1.Load(fields(5))
         End If
     End Sub
 End Class
