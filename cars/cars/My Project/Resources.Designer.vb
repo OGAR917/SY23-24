a94691cada359e18ea4a89e99f117675919562cd
KaiIsPeterGriffin <kedup432@learner.dvusd.org>
2024-03-21 13:54:22 -0700

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 3bfc99cd72dd594eb1ddf43abe51e42abc84f318.

--- cars/cars/My Project/Resources.Designer.vb
+++ cars/cars/My Project/Resources.Designer.vb
@@ -63,15 +63,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-<<<<<<< HEAD
         Friend ReadOnly Property batteryrecharger() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("batteryrecharger", resourceCulture)
-=======
-        Friend ReadOnly Property camaro() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("camaro", resourceCulture)
->>>>>>> main
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
@@ -79,15 +73,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-<<<<<<< HEAD
         Friend ReadOnly Property batteryrecharger1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("batteryrecharger1", resourceCulture)
-=======
-        Friend ReadOnly Property corvette() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("corvette", resourceCulture)
->>>>>>> main
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
@@ -95,7 +83,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-<<<<<<< HEAD
         Friend ReadOnly Property car_jack() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("car jack", resourceCulture)
@@ -139,11 +126,6 @@ Namespace My.Resources
         Friend ReadOnly Property wrenchs() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("wrenchs", resourceCulture)
-=======
-        Friend ReadOnly Property hellcat() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("hellcat", resourceCulture)
->>>>>>> main
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
