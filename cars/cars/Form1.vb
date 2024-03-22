18c6eddf0cb2c3bece5e99a1ffbb83763d8525d4
KaiIsPeterGriffin <kedup432@learner.dvusd.org>
2024-03-22 12:39:33 -0700

    Form Codes v.1

--- cars/cars/Form1.vb
+++ cars/cars/Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports System.Reflection.Emit
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim fInv As New inventory
+        fInv.ShowDialog()
+        Me.Show()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Dim f2 As New Form2
+        f2.ShowDialog()
+        Me.Show()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+        Dim fInv As New inventory
+        fInv.ShowDialog()
+        Me.Show()
+    End Sub
 End Class
