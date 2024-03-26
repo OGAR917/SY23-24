edba6d1c33799821705515340f81a8ae82827944
Oscar <ogar917@learner.dvusd.org>
2024-03-26 13:39:14 -0700

    Added Z06 image on Z06 form

--- cars/cars/corvette.Designer.vb
+++ cars/cars/corvette.Designer.vb
@@ -22,16 +22,46 @@ Partial Class corvette
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Cars.My.Resources.Resources.corvette
+        Me.PictureBox1.Location = New System.Drawing.Point(25, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(432, 310)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Stencil", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(138, 352)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(206, 33)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Corvette Z06"
         '
         'corvette
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1105, 550)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "corvette"
         Me.Text = "corvette"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
--- cars/cars/inventory.vb
+++ cars/cars/inventory.vb
@@ -1,3 +1,22 @@
 ﻿Public Class inventory
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim c8 As New corvette
+        c8.ShowDialog()
+        Me.Show()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Dim hc As New hellcat
+        hc.ShowDialog()
+        Me.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+        Dim shelby As New shelby
+        shelby.ShowDialog()
+        Me.Show()
+    End Sub
 End Class
